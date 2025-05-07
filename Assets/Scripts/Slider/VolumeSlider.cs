37244134b35e97be39b44d8e408c0ab9e8fa9319
Vadanchik <vadanchik2000@gmail.com>
2025-05-07 14:10:36 +0300

    Fixed

--- Assets/Scripts/Slider/VolumeSlider.cs
+++ Assets/Scripts/Slider/VolumeSlider.cs
@@ -7,10 +7,11 @@ public class VolumeSlider : SliderEvent
     [SerializeField] private MixerData.MixerGroup _group;
 
     private const float Ñoefficient = 0.25f;
+    private const float MaxSliderValue = 1f;
 
     protected override void HandleSliderChange(float value)
     {
         base.HandleSliderChange(value);
-        _mixer.audioMixer.SetFloat(MixerData.EnumToString(_group), Mathf.Log10(Mathf.Clamp01(value)) * (MixerData.Params.MaxVolume - MixerData.Params.ZeroVolume) * Ñoefficient + MixerData.Params.MaxVolume);
+        _mixer.audioMixer.SetFloat(MixerData.EnumToString(_group), Mathf.Log10(Mathf.Clamp(value, Mathf.Epsilon, MaxSliderValue)) * (MixerData.Params.MaxVolume - MixerData.Params.ZeroVolume) * Ñoefficient + MixerData.Params.MaxVolume);
     }
 }
